957141c90f57fbc86e61f6989c77ed74d0ea8bbc
mlinhho <mailinhtranho8@gmail.com>
2023-03-11 00:37:13 +0000

    complete

--- src/app.js
+++ src/app.js
@@ -2,10 +2,64 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// window.onload = function() {
+//write your code here
+window.onload = () => {
+  // document.querySelector(".card").classList.add(generateRandomSuit());
+  generateRandomSuit();
+  document.querySelector(".number").innerHTML = generateRandomNumber();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let number = Math.floor(Math.random() * numbers.length);
+  return numbers[number];
+};
+
+let generateRandomSuit = () => {
+  let suits = [
+    {
+      textContent: "♦️",
+      color: "red"
+    },
+    {
+      textContent: "♥️",
+      color: "red"
+    },
+    {
+      textContent: "♣️",
+      color: "black"
+    },
+    {
+      textContent: "♠️",
+      color: "black"
+    }
+  ];
+  let randomIndex = Math.floor(Math.random() * suits.length);
+  let selectedSuit = suits[randomIndex];
+  // grab the suit top and bottom
+  let suitTop = document.querySelector(".top");
+  let suitBottom = document.querySelector(".bottom");
+  // update the content of suit top and bottom
+  suitTop.textContent = selectedSuit.textContent;
+  suitBottom.textContent = selectedSuit.textContent;
+  // update the color for each top and bottom suit div
+  suitTop.style.color = selectedSuit.color;
+  suitBottom.style.color = selectedSuit.color;
+  // rotate bottom suit
+  suitBottom.style.transform = "rotate(180deg)";
 };
